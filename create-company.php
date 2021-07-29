4c7f572ec5b88d3c285ee5ff9a6e6ad4e74ce9f5
shahzad <shahzadnazar116@gmail.com>
2021-07-29 10:20:38 +0500

    Update create-company.php

--- create-company.php
+++ create-company.php
@@ -132,118 +132,42 @@
                 </div>
             </div>
             <div class="ibox-content">
+                            <input type="text" class="form-control form-control-sm m-b-xs" id="filter"
+                                   placeholder="Search in table">
 
-                <table class="footable table table-stripped toggle-arrow-tiny">
-                    <thead>
-                    <tr>
-
-                        <th data-toggle="true">Project</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th data-hide="all">Company</th>
-                        <th data-hide="all">Completed</th>
-                        <th data-hide="all">Task</th>
-                        <th data-hide="all">Date</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Project - This is example of project</td>
-                        <td>Patrick Smith</td>
-                        <td>0800 051213</td>
-                        <td>Inceptos Hymenaeos Ltd</td>
-                        <td><span class="pie">0.52/1.561</span></td>
-                        <td>20%</td>
-                        <td>Jul 14, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Alpha project</td>
-                        <td>Alice Jackson</td>
-                        <td>0500 780909</td>
-                        <td>Nec Euismod In Company</td>
-                        <td><span class="pie">6,9</span></td>
-                        <td>40%</td>
-                        <td>Jul 16, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Betha project</td>
-                        <td>John Smith</td>
-                        <td>0800 1111</td>
-                        <td>Erat Volutpat</td>
-                        <td><span class="pie">3,1</span></td>
-                        <td>75%</td>
-                        <td>Jul 18, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Gamma project</td>
-                        <td>Anna Jordan</td>
-                        <td>(016977) 0648</td>
-                        <td>Tellus Ltd</td>
-                        <td><span class="pie">4,9</span></td>
-                        <td>18%</td>
-                        <td>Jul 22, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Alpha project</td>
-                        <td>Alice Jackson</td>
-                        <td>0500 780909</td>
-                        <td>Nec Euismod In Company</td>
-                        <td><span class="pie">6,9</span></td>
-                        <td>40%</td>
-                        <td>Jul 16, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Project
-                            <small>This is example of project</small>
-                        </td>
-                        <td>Patrick Smith</td>
-                        <td>0800 051213</td>
-                        <td>Inceptos Hymenaeos Ltd</td>
-                        <td><span class="pie">0.52/1.561</span></td>
-                        <td>20%</td>
-                        <td>Jul 14, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Gamma project</td>
-                        <td>Anna Jordan</td>
-                        <td>(016977) 0648</td>
-                        <td>Tellus Ltd</td>
-                        <td><span class="pie">4,9</span></td>
-                        <td>18%</td>
-                        <td>Jul 22, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Project
-                            <small>This is example of project</small>
-                        </td>
-                        <td>Patrick Smith</td>
-                        <td>0800 051213</td>
-                        <td>Inceptos Hymenaeos Ltd</td>
-                        <td><span class="pie">0.52/1.561</span></td>
-                        <td>20%</td>
-                        <td>Jul 14, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <td colspan="5">
-                            <ul class="pagination float-right"></ul>
-                        </td>
-                    </tr>
-                    </tfoot>
-                </table>
-
-            </div>
+                            <table class="footable table table-stripped" data-page-size="8" data-filter=#filter>
+                                <thead>
+                                <tr>
+                                    <th>Company Names</th>
+                                    <th>Chat</th>
+                                    <th>Scan CQ Scan</th>
+                                    
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="gradeX">
+                                    <td>aaa</td>
+                                    <td>join</td>
+                                    <td>Scan<td>
+                                   
+                                </tr>
+                                <tr class="gradeX">
+                                    <td>aaa</td>
+                                    <td>join</td>
+                                    <td>Scan<td>
+                                </tr>
+                            
+                              
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="5">
+                                        <ul class="pagination float-right"></ul>
+                                    </td>
+                                </tr>
+                                </tfoot>
+                            </table>
+                        </div>
         </div>
     </div>
 
