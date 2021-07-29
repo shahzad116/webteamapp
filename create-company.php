08778e12347b93e9a2655f5b9948c57c599f3415
shahzad <shahzadnazar116@gmail.com>
2021-07-29 10:27:28 +0500

    new

--- create-company.php
+++ create-company.php
@@ -67,7 +67,7 @@
                 <div class="col-lg-6">
                     <div class="ibox ">
                         <div class="ibox-title">
-                            <h5>Create Proect</h5>
+                            <h5>Create Project</h5>
                             
                         </div>
                         <div class="ibox-content">
@@ -120,12 +120,7 @@
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-wrench"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#" class="dropdown-item">Config option 1</a>
-                        </li>
-                        <li><a href="#" class="dropdown-item">Config option 2</a>
-                        </li>
-                    </ul>
+                  
                     <a class="close-link">
                         <i class="fa fa-times"></i>
                     </a>
@@ -184,130 +179,49 @@
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-wrench"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#" class="dropdown-item">Config option 1</a>
-                        </li>
-                        <li><a href="#" class="dropdown-item">Config option 2</a>
-                        </li>
-                    </ul>
+                    
                     <a class="close-link">
                         <i class="fa fa-times"></i>
                     </a>
                 </div>
             </div>
             <div class="ibox-content">
+                            <input type="text" class="form-control form-control-sm m-b-xs" id="filter"
+                                   placeholder="Search in table">
 
-                <table class="footable table table-stripped toggle-arrow-tiny">
-                    <thead>
-                    <tr>
-
-                        <th data-toggle="true">Project</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th data-hide="all">Company</th>
-                        <th data-hide="all">Completed</th>
-                        <th data-hide="all">Task</th>
-                        <th data-hide="all">Date</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Project - This is example of project</td>
-                        <td>Patrick Smith</td>
-                        <td>0800 051213</td>
-                        <td>Inceptos Hymenaeos Ltd</td>
-                        <td><span class="pie">0.52/1.561</span></td>
-                        <td>20%</td>
-                        <td>Jul 14, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Alpha project</td>
-                        <td>Alice Jackson</td>
-                        <td>0500 780909</td>
-                        <td>Nec Euismod In Company</td>
-                        <td><span class="pie">6,9</span></td>
-                        <td>40%</td>
-                        <td>Jul 16, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Betha project</td>
-                        <td>John Smith</td>
-                        <td>0800 1111</td>
-                        <td>Erat Volutpat</td>
-                        <td><span class="pie">3,1</span></td>
-                        <td>75%</td>
-                        <td>Jul 18, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Gamma project</td>
-                        <td>Anna Jordan</td>
-                        <td>(016977) 0648</td>
-                        <td>Tellus Ltd</td>
-                        <td><span class="pie">4,9</span></td>
-                        <td>18%</td>
-                        <td>Jul 22, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Alpha project</td>
-                        <td>Alice Jackson</td>
-                        <td>0500 780909</td>
-                        <td>Nec Euismod In Company</td>
-                        <td><span class="pie">6,9</span></td>
-                        <td>40%</td>
-                        <td>Jul 16, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Project
-                            <small>This is example of project</small>
-                        </td>
-                        <td>Patrick Smith</td>
-                        <td>0800 051213</td>
-                        <td>Inceptos Hymenaeos Ltd</td>
-                        <td><span class="pie">0.52/1.561</span></td>
-                        <td>20%</td>
-                        <td>Jul 14, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Gamma project</td>
-                        <td>Anna Jordan</td>
-                        <td>(016977) 0648</td>
-                        <td>Tellus Ltd</td>
-                        <td><span class="pie">4,9</span></td>
-                        <td>18%</td>
-                        <td>Jul 22, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    <tr>
-                        <td>Project
-                            <small>This is example of project</small>
-                        </td>
-                        <td>Patrick Smith</td>
-                        <td>0800 051213</td>
-                        <td>Inceptos Hymenaeos Ltd</td>
-                        <td><span class="pie">0.52/1.561</span></td>
-                        <td>20%</td>
-                        <td>Jul 14, 2013</td>
-                        <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                    </tr>
-                    
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <td colspan="5">
-                            <ul class="pagination float-right"></ul>
-                        </td>
-                    </tr>
-                    </tfoot>
-                </table>
-
-            </div>
+                            <table class="footable table table-stripped" data-page-size="8" data-filter=#filter>
+                                <thead>
+                                <tr>
+                                    <th>Project Names</th>
+                                    <th>Manager</th>
+                                    <th>Start Date</th>
+                                    
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="gradeX">
+                                    <td>aaa</td>
+                                    <td>join</td>
+                                    <td>Scan<td>
+                                   
+                                </tr>
+                                <tr class="gradeX">
+                                    <td>aaa</td>
+                                    <td>join</td>
+                                    <td>Scan<td>
+                                </tr>
+                            
+                              
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="5">
+                                        <ul class="pagination float-right"></ul>
+                                    </td>
+                                </tr>
+                                </tfoot>
+                            </table>
+                        </div>
         </div>
     </div>
 </div>
