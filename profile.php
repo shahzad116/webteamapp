933e53adfad6ae07adf0525ee385e8c0ce37f493
shahzad <shahzadnazar116@gmail.com>
2021-08-05 15:09:43 +0500

    profile added

---
+++ profile.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> Profile</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+    <?php include 'pages/header.php';?>
+            <div class="row wrapper border-bottom white-bg page-heading">
+                <div class="col-lg-10">
+                    <h2>Profile</h2>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="dashboard.php">Home</a>
+                        </li>
+                        
+                        <li class="breadcrumb-item active">
+                            <strong>Profile</strong>
+                        </li>
+                    </ol>
+                </div>
+                <div class="col-lg-2">
+
+                </div>
+            </div>
+        <div class="wrapper wrapper-content">
+            <div class="row animated fadeInRight">
+                <div class="col-md-4">
+                    <div class="ibox ">
+                        <div class="ibox-title">
+                            <h5>Profile Detail</h5>
+                        </div>
+                        <div>
+                        <div class="ibox-content text-center">
+                                <h1>Nicki Smith</h1>
+                                <div class="m-b-sm">
+                                        <img alt="image" class="rounded-circle" src="img/a8.jpg">
+                                </div>
+                                        <p class="font-bold">Consectetur adipisicing</p>
+
+                              
+                            </div>
+                            <div class="ibox-content profile-content">
+                                
+                                <p><i class="fa fa-map-marker"></i> Riviera State 32/106</p>
+                                <h5>
+                                    About me
+                                </h5>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat.
+                                </p>
+                                <div class="row m-t-lg">
+                                    <div class="col-md-4">
+                                        <span class="bar">5,3,9,6,5,9,7,3,5,2</span>
+                                        <h5><strong>169</strong> Posts</h5>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <span class="line">5,3,9,6,5,9,7,3,5,2</span>
+                                        <h5><strong>28</strong> Following</h5>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <span class="bar">5,3,2,-1,-3,-2,2,3,5,2</span>
+                                        <h5><strong>240</strong> Followers</h5>
+                                    </div>
+                                </div>
+                               
+                                <div class="ibox">
+                        <div class="ibox-content">
+                            <h3>Private message</h3>
+
+                            <p class="small">
+                                Send private message to Alex Smith
+                            </p>
+
+                            <div class="form-group">
+                                <label>Subject</label>
+                                <input type="email" class="form-control" placeholder="Message subject">
+                            </div>
+                            <div class="form-group"><label>Employee</label> 
+                                    <select class="form-control m-b" name="totalusers">
+                                        <option>Select</option>
+                                        <option value="murat">Murat</option>
+                                        <option value="amir">Amir</option>
+                                    </select>
+                                </div>
+                            <div class="form-group">
+                                <label>Message</label>
+                                <textarea class="form-control" placeholder="Your message" rows="3"></textarea>
+                            </div>
+                            <button class="btn btn-primary btn-block">Send</button>
+
+                        </div>
+                    </div>
+
+                            </div>
+                    </div>
+                </div>
+                    </div>
+                <div class="col-md-8">
+                    <div class="ibox ">
+                        <div class="ibox-title">
+                            <h5>My Projects</h5>
+                            <div class="ibox-tools">
+                                <a class="collapse-link">
+                                    <i class="fa fa-chevron-up"></i>
+                                </a>
+                               
+                               
+                               
+                            </div>
+                        </div>
+                        <div class="ibox-content">
+
+
+                            <div class="ibox-content">
+                            <div class="row m-b-sm m-t-sm">
+                                <div class="col-md-1">
+                                    <button type="button" id="loading-example-btn" class="btn btn-white btn-sm"><i class="fa fa-refresh"></i> Refresh</button>
+                                </div>
+                                <div class="col-md-11">
+                                    <div class="input-group"><input type="text" placeholder="Search" class="form-control-sm form-control"> <span class="input-group-btn">
+                                        <button type="button" class="btn btn-sm btn-primary"> Go!</button> </span></div>
+                                </div>
+                            </div>
+
+                            <div class="project-list">
+
+                                <table class="table table-hover">
+                                    <tbody>
+                                    <tr>
+                                        <td class="project-status">
+                                            <span class="label label-primary">Active</span>
+                                        </td>
+                                        <td class="project-title">
+                                            <a href="project_detail.php">Contract with Zender Company</a>
+                                            <br>
+                                            <small>Created 14.08.2014</small>
+                                        </td>
+                                        <td class="project-completion">
+                                                <small>Completion with: 48%</small>
+                                                <div class="progress progress-mini">
+                                                    <div style="width: 48%;" class="progress-bar"></div>
+                                                </div>
+                                        </td>
+                                        <td class="project-people">
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a3.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a1.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a2.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a4.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a5.jpg"></a>
+                                        </td>
+                                        <td class="project-actions">
+                                            <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> View </a>
+                                            <a href="#" class="btn btn-white btn-sm"><i class="fa fa-pencil"></i> Edit </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="project-status">
+                                            <span class="label label-primary">Active</span>
+                                        </td>
+                                        <td class="project-title">
+                                            <a href="project_detail.php">There are many variations of passages</a>
+                                            <br>
+                                            <small>Created 11.08.2014</small>
+                                        </td>
+                                        <td class="project-completion">
+                                            <small>Completion with: 28%</small>
+                                            <div class="progress progress-mini">
+                                                <div style="width: 28%;" class="progress-bar"></div>
+                                            </div>
+                                        </td>
+                                        <td class="project-people">
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a7.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a6.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a3.jpg"></a>
+                                        </td>
+                                        <td class="project-actions">
+                                            <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> View </a>
+                                            <a href="#" class="btn btn-white btn-sm"><i class="fa fa-pencil"></i> Edit </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="project-status">
+                                            <span class="label label-default">Unactive</span>
+                                        </td>
+                                        <td class="project-title">
+                                            <a href="project_detail.php">Many desktop publishing packages and web</a>
+                                            <br>
+                                            <small>Created 10.08.2014</small>
+                                        </td>
+                                        <td class="project-completion">
+                                            <small>Completion with: 8%</small>
+                                            <div class="progress progress-mini">
+                                                <div style="width: 8%;" class="progress-bar"></div>
+                                            </div>
+                                        </td>
+                                        <td class="project-people">
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a5.jpg"></a>
+                                            <a href=""><img alt="image" class="rounded-circle" src="img/a3.jpg"></a>
+                                        </td>
+                                        <td class="project-actions">
+                                            <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> View </a>
+                                            <a href="#" class="btn btn-white btn-sm"><i class="fa fa-pencil"></i> Edit </a>
+                                        </td>
+                                    </tr>
+                               
+                                 
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="float-right">
+                10GB of <strong>250GB</strong> Free.
+            </div>
+            <div>
+                <strong>Copyright</strong> Example Company &copy; 2014-2018
+            </div>
+        </div>
+
+        </div>
+        </div>
+
+
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <script src="js/plugins/pace/pace.min.js"></script>
+
+    <!-- Peity -->
+    <script src="js/plugins/peity/jquery.peity.min.js"></script>
+
+    <!-- Peity -->
+    <script src="js/demo/peity-demo.js"></script>
+
+</body>
+
+</html>
